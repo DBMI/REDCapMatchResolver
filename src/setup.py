14c770c8b17d4723e5ecb2c2fb96cdc33c9636f5
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-11-22 12:05:22 -0800

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='REDCapMatchResolver',
+    version='0.1',
+    packages=['redcapmatchresolver'],
+    package_dir={'': 'src'},
+    url='https://github.com/DBMI/REDCapMatchResolver',
+    license='',
+    author='Kevin J. Delaney',
+    author_email='kjdelaney@ucsd.edu',
+    description='Assists with human expert review of possible patient matches.'
+)
