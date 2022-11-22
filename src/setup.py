963c9c86112ddbc8c0011591fe4017c790713a4f
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-11-22 14:19:03 -0800

    Correcting package name.

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='REDCapMatchResolver',
-    version='0.1',
+    version='0.1.1',
     packages=['redcapmatchresolver'],
     package_dir={'': 'src'},
     url='https://github.com/DBMI/REDCapMatchResolver',
