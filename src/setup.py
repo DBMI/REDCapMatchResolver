a410b3459590eddabb4ffafe54d377bb15569aae
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-11-23 09:21:16 -0800

    Account for header in match reports.

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='REDCapMatchResolver',
-    version='0.1.2',
-    packages=['redcapmatchresolver'],
-    package_dir={'': 'src'},
-    url='https://github.com/DBMI/REDCapMatchResolver',
-    license='',
-    author='Kevin J. Delaney',
-    author_email='kjdelaney@ucsd.edu',
-    description='Assists with human expert review of possible patient matches.'
+    name="REDCapMatchResolver",
+    version="0.1.3",
+    packages=["redcapmatchresolver"],
+    package_dir={"": "src"},
+    url="https://github.com/DBMI/REDCapMatchResolver",
+    license="",
+    author="Kevin J. Delaney",
+    author_email="kjdelaney@ucsd.edu",
+    description="Assists with human expert review of possible patient matches.",
 )
