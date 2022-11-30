e1fa0256e3cf58ceea55fb18fd13c4f20204f67a
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-11-30 10:46:14 -0800

    1) Add reasons why patients are not the same. 2) Add 'best_appointment' feature.

--- src/redcapmatchresolver/redcap_report_writer.py
+++ src/redcapmatchresolver/redcap_report_writer.py
@@ -3,7 +3,6 @@ Module: contains class REDCapReportWriter
 used to produce list of patient matches to be reviewed.
 """
 import logging
-import os
 from .utilities import Utilities
 
 
@@ -11,6 +10,16 @@ class REDCapReportWriter:  # pylint: disable=logging-fstring-interpolation
     """
     Produces formatted patient report.
     """
+
+    addendum = (
+        "CRC Review: ABOVE (↑) patients are\n"
+        "    ☐ Same\n"
+        "    ☐ NOT Same: Family members\n"
+        "    ☐ NOT Same: Living at same address\n"
+        "    ☐ NOT Same: Parent & child\n"
+        "    ☐ NOT Same: Other\n"
+        "Notes:...................................................\n"
+    )
 
     def __init__(self, report_filename: str = "patient_report.txt"):
         """
@@ -22,37 +31,61 @@ class REDCapReportWriter:  # pylint: disable=logging-fstring-interpolation
         self.__log = logging.getLogger(__name__)
         self.__report_filename = report_filename
         Utilities.ensure_output_path(self.__report_filename)
+        self.__reports = []
 
-    def add_match(self, match: str = None) -> bool:
+    def add_match(self, match: str = None) -> None:
         """Allows external code to add a pre-formatted text block comparing two patient records.
 
         Parameters
         ----------
-        match str Pre-formatted block of text
+        match : str     Pre-formatted block of text
+        """
+        if match is not None and isinstance(match, str):
+            self.__reports.append(match)
+
+    def report_filename(self) -> str:
+        """Allows external code to ask where the file went.
 
         Returns
         -------
-        success bool Did it work or not?
+        filename : str
         """
-        success = False
-        addendum = (
-            """CRC Review: ABOVE (↑) patients are\n    ☐ Same\n    ☐ NOT Same\n"""
-        )
+        return self.__report_filename
 
-        if match is not None and isinstance(match, str):
-            try:
-                with (
-                    open(self.__report_filename, mode="a", encoding="utf-8")
-                ) as file_obj:
-                    num_char_written = file_obj.write(match + addendum)
-                    success = num_char_written == len(match) + len(addendum)
-            except Exception as file_write_error:  # pragma: no cover
-                self.__log.error(
-                    f"Unable to write match to log because {str(file_write_error)}."
+    def write(self) -> bool:
+        """Writes out the accumulated match reports, assigning a sequential number to each one.
+
+        Returns
+        -------
+        success : bool
+        """
+        match_index = 0
+        total_number_of_match_reports = len(self.__reports)
+
+        with (open(self.__report_filename, mode="a", encoding="utf-8")) as file_obj:
+            for match in self.__reports:
+                record_numbering_line = (
+                    f"Record {match_index} of {total_number_of_match_reports}\n"
                 )
-                raise
+                this_record = (
+                    match + record_numbering_line + REDCapReportWriter.addendum
+                )
 
-        return success
+                try:
+                    num_char_written = file_obj.write(this_record)
+
+                    if num_char_written != len(this_record):
+                        return False  # pragma: no cover
+
+                except Exception as file_write_error:  # pragma: no cover
+                    self.__log.error(
+                        f"Unable to write match to log because {str(file_write_error)}."
+                    )
+                    raise
+
+                match_index += 1
+
+        return True
 
 
 if __name__ == "__main__":
