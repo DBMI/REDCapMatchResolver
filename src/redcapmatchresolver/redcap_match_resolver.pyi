142d0f832637f119af551b5536a5543bd85bcc25
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-02-03 13:31:00 -0800

    Fix path problems.

--- src/redcapmatchresolver/redcap_match_resolver.pyi
+++ src/redcapmatchresolver/redcap_match_resolver.pyi
@@ -1,7 +1,39 @@
-from .redcap_report_reader import CrcReview as CrcReview, REDCapReportReader as REDCapReportReader
-from .utilities import Utilities as Utilities
+import sqlite3
+from typing import Union
+
+from .redcap_report_reader import CrcReview as CrcReview
 
 class REDCapMatchResolver:
-    def __init__(self, db_filename: str = ...) -> None: ...
+    def __init__(self, db_filename: str = ...) -> None:
+        self.__dataframe_fields_list = None
+        self.__database_fields_list = None
+        self.__conn = None
+        self.__redcap_reader = None
+        self.__log = None
+        ...
     def lookup_potential_match(self, match_block: str) -> CrcReview: ...
     def read_reports(self, import_folder: str) -> bool: ...
+    def _is_connected(self, conn: Union[sqlite3.Connection, None]):
+        pass
+    def _reader_ready(self):
+        pass
+    def _insert_report(self, report_df):
+        pass
+    def _build_required_fields(self):
+        pass
+    def _setup_db(self, db_filename):
+        pass
+    def _build_decision_table(self, connection):
+        pass
+    def _create_connection(self, db_filename):
+        pass
+    def _create_matches_table(self, connection):
+        pass
+    def _create_decisions_table(self, conn):
+        pass
+    def _drop_decisions_table(self, conn):
+        pass
+    def _drop_matches_table(self, conn):
+        pass
+    def _translate_crc_decision(self, crc_decision_string):
+        pass
--- src/redcapmatchresolver/redcap_report_reader.pyi
+++ src/redcapmatchresolver/redcap_report_reader.pyi
@@ -1,13 +1,12 @@
-import pandas
-from .utilities import Utilities as Utilities
-from _typeshed import Incomplete
 from enum import Enum
-from typing import NamedTuple
+from typing import NamedTuple, Union
+
+import pandas  # type: ignore[import]
 
 class ReportLine(NamedTuple):
-    name: Incomplete
-    epic_value: Incomplete
-    redcap_value: Incomplete
+    name: str
+    epic_value: str
+    redcap_value: str
 
 class CrcReason(Enum):
     FAMILY: int
@@ -28,8 +27,35 @@ class CrcReview(Enum):
     def __lt__(self, other: object) -> bool: ...
 
 class REDCapReportReader:
-    def __init__(self) -> None: ...
+    __separator = None
+
+    def __init__(self) -> None:
+        self.__row_index = None
+        self.__report_contents = None
+        self.__log = None
+        ...
     @staticmethod
     def convert_nulls(value: str) -> Union[str, None]: ...
     def read_file(self, report_filename: str) -> pandas.DataFrame: ...
     def read_text(self, block_txt: str) -> pandas.DataFrame: ...
+    @classmethod
+    def _break_into_pieces(cls, data_line):
+        pass
+    @classmethod
+    def _find_column(cls, next_line, param):
+        pass
+    @classmethod
+    def _parse_line(cls, data_line, epic_index, redcap_index):
+        pass
+    def _read(self):
+        pass
+    def _open_file(self, report_filename):
+        pass
+    def _open_text(self, block_txt):
+        pass
+    def _next_line(self):
+        pass
+    def _read_crc_decision(self):
+        pass
+    def _at_end(self):
+        pass
