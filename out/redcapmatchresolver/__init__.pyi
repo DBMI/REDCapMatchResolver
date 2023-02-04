32044da9658d675afbc6e0c5ab75f55f45af58fa
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-02-03 16:13:50 -0800

    Delete __init__.pyi

--- out/redcapmatchresolver/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from .redcap_appointment import REDCapAppointment as REDCapAppointment
-from .redcap_clinic import REDCapClinic as REDCapClinic
-from .redcap_match_resolver import REDCapMatchResolver as REDCapMatchResolver
-from .redcap_patient import REDCapPatient as REDCapPatient
-from .redcap_report_reader import REDCapReportReader as REDCapReportReader
-from .redcap_report_writer import REDCapReportWriter as REDCapReportWriter
-from .utilities import Utilities as Utilities
