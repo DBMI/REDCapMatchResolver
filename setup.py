55676d6176c32b056bbd139e7edaf33d07442cbb
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2023-07-14 12:43:00 -0700

    Improve `decisions` table.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="REDCapMatchResolver",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     package_dir={"src": "src"},
     include_package_data=True,
