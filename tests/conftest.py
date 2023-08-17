94967c7c68fb615782a1d1f771c71c73c9b82026
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2023-08-17 11:18:31 -0700

    Update wobbler report format.

--- tests/conftest.py
+++ tests/conftest.py
@@ -178,14 +178,14 @@ def fixture_matching_patients() -> str:
     ---------------
     Study ID: 1234
     PAT_ID: A56789
-    Common Name         Epic Value             RedCap Value          Score
-    C_MRN               123                    123                   1.0
-    C_FIRST             John                   Jon                   0.8
-    C_LAST              Smith                  Smith                 1.0
-    C_DOB               2022-11-01             Nov 1, 2022           1.0
-    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com     1.0
-    C_ADDR_CALCULATED   1313 Mockingbird Lane  1313 Mockingbird Ln   1.0
-    C_PHONE_CALCULATED  800-555-1212           800-555-1212          1.0
+    Common Name         Epic Value             RedCap Value          Match Quality
+    C_MRN               123                    123                   [exact]
+    C_FIRST             John                   Jon                   [-]
+    C_LAST              Smith                  Smith                 [exact]
+    C_DOB               2022-11-01             Nov 1, 2022           [exact]
+    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com     [alphanum]
+    C_ADDR_CALCULATED   1313 Mockingbird Lane  1313 Mockingbird Ln   [exact]
+    C_PHONE_CALCULATED  800-555-1212           800-555-1212          [exact]
     ---------------
     """
 
@@ -203,13 +203,13 @@ def fixture_malformed_match_block() -> str:
     ---------------
     Study ID: 1234
     Record: 1 of 1000
-    Common Name         Epic Value             RedCap Value         Score
-    C_MRN               1234                   1235                 0.7
-    C_FIRST             Johnnie                Jonfen               0.8
-    C_LAST              Schmidt                Schmidt              1.0
-    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com    1.0
-    C_ADDR_CALCULATED   4 Privet Drive         4 Privet Drive       1.0
-    C_PHONE_CALCULATED  800-555-1212           800-555-1212         1.0
+    Common Name         Epic Value             RedCap Value         Match Quality
+    C_MRN               1234                   1235                 [-]
+    C_FIRST             Johnnie                Jonfen               [-]
+    C_LAST              Schmidt                Schmidt              [exact]
+    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com    [alphanum]
+    C_ADDR_CALCULATED   4 Privet Drive         4 Privet Drive       [exact]
+    C_PHONE_CALCULATED  800-555-1212           800-555-1212         [exact]
     ---------------
     """
 
@@ -222,14 +222,14 @@ def fixture_non_matching_patients() -> str:
     Study ID: 1234
     PAT_ID: A00000
     Record: 1 of 1000
-    Common Name         Epic Value             RedCap Value          Score
-    C_MRN               1234                   1235                  0.7
-    C_FIRST             Johnnie                Jonfen                0.8
-    C_LAST              Schmidt                Schmidt               1.0
-    C_DOB               2022-11-18             Nov 18, 2022          1.0
-    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com     1.0
-    C_ADDR_CALCULATED   4 Privet Drive         4 Privet Drive        1.0
-    C_PHONE_CALCULATED  800-555-1212           800-555-1212          1.0
+    Common Name         Epic Value             RedCap Value          Match Quality
+    C_MRN               1234                   1235                  [-]
+    C_FIRST             Johnnie                Jonfen                [-]
+    C_LAST              Schmidt                Schmidt               [exact]
+    C_DOB               2022-11-18             Nov 18, 2022          [exact]
+    C_EMAIL             jsmith@yahoo.com       j.smith@gmail.com     [alphanum]
+    C_ADDR_CALCULATED   4 Privet Drive         4 Privet Drive        [exact]
+    C_PHONE_CALCULATED  800-555-1212           800-555-1212          [exact]
     ---------------
     """
 
