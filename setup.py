cc718f6c392447b7a309e5cc63459d40f749fce7
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2023-08-17 07:46:58 -0700

    Remove `weight` from `MatchRecord`, `MatchVariable` and `CommonField` classes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="REDCapMatchResolver",
-    version="1.2.7",
+    version="1.2.8",
     packages=find_packages(),
     package_dir={"src": "src"},
     include_package_data=True,
