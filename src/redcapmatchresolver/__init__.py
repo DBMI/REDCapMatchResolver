4f1ea8ac7918eac61f360d4e0026ba0248617403
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2023-08-18 15:44:31 -0700

    Fix imports.

--- src/redcapmatchresolver/__init__.py
+++ src/redcapmatchresolver/__init__.py
@@ -20,10 +20,10 @@ Classes:
     REDCap Report Reader
     REDCap Report Writer
 """
-from match_records import CommonField, MatchRecord, MatchTuple, MatchVariable
-from redcap_appointment import REDCapAppointment
-from redcap_clinic import REDCapClinic
-from redcap_match_resolver import REDCapMatchResolver
-from redcap_patient import REDCapPatient
-from redcap_report_reader import REDCapReportReader
-from redcap_report_writer import REDCapReportWriter
+from .match_records import CommonField, MatchRecord, MatchTuple, MatchVariable
+from .redcap_appointment import REDCapAppointment
+from .redcap_clinic import REDCapClinic
+from .redcap_match_resolver import REDCapMatchResolver
+from .redcap_patient import REDCapPatient
+from .redcap_report_reader import REDCapReportReader
+from .redcap_report_writer import REDCapReportWriter
