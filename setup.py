8a48ef0b06d8920501f862a98576063da4227fe2
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2023-08-18 15:54:54 -0700

    Issue new version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="REDCapMatchResolver",
-    version="1.3.0",
+    version="1.3.1",
     packages=find_packages(),
     package_dir={"src": "src"},
     include_package_data=True,
