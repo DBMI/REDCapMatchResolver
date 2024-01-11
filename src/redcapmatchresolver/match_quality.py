45e3b5a6b78bf5687e0cfd2f62e8942e221d22b6
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2024-01-11 14:36:03 -0800

    Move MatchQuality to REDCapMatchResolver

---
+++ src/redcapmatchresolver/match_quality.py
@@ -0,0 +1,116 @@
+"""
+Module: contains MatchQuality class.
+"""
+from enum import Enum
+
+
+class MatchQuality(Enum):
+    """Various quality of matches (in terms of any particular field)"""
+
+    IGNORED = -10
+    MATCHED_NOPE = -1
+    MATCHED_NULL = 0
+    MATCHED_EXACT = 1
+    MATCHED_CASE_INSENSITIVE = 2
+    MATCHED_ALPHA_NUM = 3
+    MATCHED_SUBSTRING = 4
+    MATCHED_FUZZY = 5
+    MATCHED_CALCULATED = 6
+
+    @classmethod
+    def convert(cls, match_str: str):
+        """Create MatchQuality object from a string.
+
+        Parameters
+        ----------
+        match_str : str
+
+        Returns
+        -------
+        match : MatchQuality object
+        """
+        if match_str is None:
+            raise TypeError("Input 'match_str' is not the expected string.")
+
+        if not isinstance(match_str, str):
+            raise TypeError("Input 'match_str' is not the expected string.")
+
+        match = MatchQuality.MATCHED_NULL
+
+        if "ignored" in match_str.lower():
+            match = MatchQuality.IGNORED
+
+        if "nope" in match_str.lower():
+            match = MatchQuality.MATCHED_NOPE
+
+        if "exact" in match_str.lower():
+            match = MatchQuality.MATCHED_EXACT
+
+        if "case_insensitive" in match_str.lower():
+            match = MatchQuality.MATCHED_CASE_INSENSITIVE
+
+        if "alpha_num" in match_str.lower():
+            match = MatchQuality.MATCHED_ALPHA_NUM
+
+        if "substring" in match_str.lower():
+            match = MatchQuality.MATCHED_SUBSTRING
+
+        if "fuzzy" in match_str.lower():
+            match = MatchQuality.MATCHED_FUZZY
+
+        if "calculated" in match_str.lower():
+            match = MatchQuality.MATCHED_CALCULATED
+
+        return match
+
+    def good_enough(self) -> bool:
+        """We'll consider all these quality of matches as "good enough" to
+        say the fields are equal.
+
+        Returns
+        -------
+        good_enough : bool
+        """
+        if not self:  # pragma: no cover
+            return False
+
+        return self.value > 0
+
+    def ignored(self) -> bool:
+        """Allows external code to test if the special case of "IGNORED" is true.
+
+        Returns
+        -------
+        ignored : bool
+        """
+        if not self:  # pragma: no cover
+            return False
+
+        return self == MatchQuality.IGNORED
+
+    def __str__(self) -> str:  # pylint: disable = too-many-return-statements
+        if self == MatchQuality.IGNORED:
+            return "ignored"
+
+        if self == MatchQuality.MATCHED_EXACT:
+            return "exact"
+
+        if self == MatchQuality.MATCHED_NULL:
+            return "null"
+
+        if self == MatchQuality.MATCHED_CASE_INSENSITIVE:
+            return "lower"
+
+        if self == MatchQuality.MATCHED_ALPHA_NUM:
+            return "alphanum"
+
+        if self == MatchQuality.MATCHED_FUZZY:
+            return "fuzzy"
+
+        if self == MatchQuality.MATCHED_CALCULATED:
+            return "calculated"
+
+        if self == MatchQuality.MATCHED_SUBSTRING:
+            return "substring"
+
+        return "-"
---
+++ tests/test_match_quality.py
@@ -0,0 +1,120 @@
+"""
+Module test_match_quality.py, which performs automated
+testing of the MatchQuality class.
+"""
+import pytest
+
+from src.redcapmatchresolver.match_quality import MatchQuality
+
+
+def test_match_quality():
+    match_quality_obj = MatchQuality.convert("IGNORED")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.IGNORED
+    assert not match_quality_obj.good_enough()
+    assert match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "ignored"
+
+    match_quality_obj = MatchQuality.convert("NOPE")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_NOPE
+    assert not match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "-"
+
+    match_quality_obj = MatchQuality.convert("null")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_NULL
+    assert not match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "null"
+
+    match_quality_obj = MatchQuality.convert("EXACT")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_EXACT
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "exact"
+
+    match_quality_obj = MatchQuality.convert("CASE_INSENSITIVE")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_CASE_INSENSITIVE
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "lower"
+
+    match_quality_obj = MatchQuality.convert("ALPHA_NUM")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_ALPHA_NUM
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "alphanum"
+
+    match_quality_obj = MatchQuality.convert("substring")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_SUBSTRING
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "substring"
+
+    match_quality_obj = MatchQuality.convert("FUZZY")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_FUZZY
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "fuzzy"
+
+    match_quality_obj = MatchQuality.convert("CALCULATED")
+    assert match_quality_obj is not None
+    assert isinstance(match_quality_obj, MatchQuality)
+    assert match_quality_obj == MatchQuality.MATCHED_CALCULATED
+    assert match_quality_obj.good_enough()
+    assert not match_quality_obj.ignored()
+    string_representation = str(match_quality_obj)
+    assert string_representation is not None
+    assert isinstance(string_representation, str)
+    assert string_representation == "calculated"
+
+
+def test_match_quality_errors():
+    with pytest.raises(TypeError):
+        MatchQuality.convert(None)
+
+    with pytest.raises(TypeError):
+        MatchQuality.convert(1979)
+
+
+if __name__ == "__main__":
+    pass
